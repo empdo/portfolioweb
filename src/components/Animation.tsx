b92c3343915d825609b640353dc9bba4a8773bf0
empdo <emil.essung@live.se>
2023-01-15 23:18:12 +0100

    add fixed interval

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,13 +1,13 @@
 import { CipherCCM, randomInt } from "crypto";
 import { disconnect } from "process";
 import { useEffect, useRef } from "react";
+import { map } from "zod";
 
 interface Circle {
   x: number;
   y: number;
   r: number;
   shouldGrow: boolean;
-  lifetime: number;
 }
 
 let circles: Circle[] = [];
@@ -21,17 +21,45 @@ const distance = (x1: number, y1: number, x2: number, y2: number) =>
 const validPos = (circle: Circle): boolean => {
   let ret = true;
   circles.forEach((c) => {
-    if (c != circle && c.r + 3 > distance(c.x, c.y, circle.x, circle.y)) {
+    if (c != circle && c.r + 1.5 > distance(c.x, c.y, circle.x, circle.y)) {
       ret = false;
     }
   });
   return ret;
 };
 
+const addCircle = (radius: number) => {
+  const maxIt = 300;
+
+  for (let i = 0; i <= maxIt; i++) {
+    let x = getRandomFloat(-1, 1);
+    const y =
+      getRandomFloat(-1, 1) * Math.sqrt(1 - x * x) * (radius - 3) + radius;
+    x = x * (radius - 3) + radius;
+
+    const circle: Circle = {
+      x,
+      y,
+      r: 0,
+      shouldGrow: true,
+    };
+
+    if (validPos(circle)) {
+      circles.push(circle);
+      break;
+    }
+
+    if (i == maxIt) {
+      //TODO: wait 10sec, then remove every buble on some interval of
+      return true;
+    }
+  }
+  return false;
+};
+
 const Animation = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   let done = false;
-  const maxIt = 150;
 
   let canvas: HTMLCanvasElement | null;
   let context: CanvasRenderingContext2D | null;
@@ -62,7 +90,6 @@ const Animation = () => {
   }, []);
 
   let frameCount = 0;
-  let animationFrameId: number;
   useEffect(() => {
     const interval = setInterval(() => {
       const render = () => {
@@ -90,30 +117,16 @@ const Animation = () => {
     ctx.arc(radius, radius, radius, 0, 2 * Math.PI);
     ctx.stroke();
 
-    for (let i = 0; i <= maxIt; i++) {
-      let x = getRandomFloat(-1, 1);
-      const y = getRandomFloat(-1, 1) * Math.sqrt(1 - x * x) * radius + radius;
-      x = x * 72 + 75;
+    const growMultiplier = 1;
+    let j = 0;
+    for (; j < (frameCount)/100; j += 1) {
+        if (frameCount % 2 == 0 || frameCount > 300) {
 
-      const circle: Circle = {
-        x,
-        y,
-        r: 1,
-        shouldGrow: true,
-        lifetime: frameCount + Math.random() * 400,
-      };
-
-      if (validPos(circle)) {
-        circles.push(circle);
-        break;
-      }
-
-      if (i == maxIt) {
-        //TODO: wait 10sec, then remove every buble on some interval of
-        done = true;
-      }
+            done = addCircle(radius);
+        }
     }
 
+    console.log(j);
     circles.forEach((c) => {
       if (c.shouldGrow) {
         //check collision
@@ -131,10 +144,10 @@ const Animation = () => {
       }
 
       if (c.shouldGrow) {
-        c.r += 0.8;
+        c.r += (0.2 * growMultiplier) / Math.max(0.01 * c.r * c.r, 0.5);
       }
 
-      ctx.moveTo(c.x + c.r, c.y);
+      ctx.beginPath();
       ctx.arc(c.x, c.y, c.r, 0, 2 * Math.PI);
       ctx.stroke();
     });
